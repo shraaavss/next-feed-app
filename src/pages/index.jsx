58d76c502cb5d474809f2b12d6e1b8e8edf4ddeb
snehguptafr <guptasneh302004@gmail.com>
2024-03-03 00:50:35 +0530

    .

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,7 @@ export default function Home() {
     fetch("https://api.capy.lol/v1/capybaras?take=25&random=true")
       .then(res => res.json())
       .then(data=> setTestArray(data.data))
-    print("Fetched")
+    console.log("Fetched")
   }, [])
 
   const cardsList = testArray.map(capy => {
