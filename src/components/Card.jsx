f733f1e66e39843f5e769425e02e8ac2757927a0
shraaavss <shravs2707@gmail.com>
2024-03-02 19:46:38 +0530

    pdding

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,35 +9,40 @@ import like from "../assets/like.svg"
 import repost from "../assets/repost.svg";
 import share from "../assets/share.svg"
 
-export default function Post(props){
+export default function Post(props) {
 
-    return(
-        <div className="post">
-            <div className="post-header">
-                <div className="post-user">
-                    <Image src={temp} width={"2rem"} height={"2rem"} alt="userImg"/>
-                    <span>Sneh Gupta</span>
+    return (
+        <div className="post-wrap">
+
+            <div className="post">
+                <div className="post-header">
+                    <div className="post-user">
+                        <Image src={temp} width={"2rem"} height={"2rem"} alt="userImg" />
+                        <span>Sneh Gupta</span>
+                    </div>
+                    <div className="post-control">
+                        <Image src={appImg} width={"2rem"} height={"2rem"} alt="appImg" />
+                        <Image src={snoozeImg} width={"2rem"} height={"2rem"} alt="snoozeImg" />
+                    </div>
                 </div>
-                <div className="post-control">
-                    <Image src={appImg} width={"2rem"} height={"2rem"} alt="appImg"/>
-                    <Image src={snoozeImg} width={"2rem"} height={"2rem"} alt="snoozeImg"/>
+
+                <div className="post-body">
+                    <Image src={test} width={600} alt="post" />
+                </div>
+
+                <div className="post-footer">
+                    <div className="post-action">
+                        <Image src={like} width={"2rem"} height={"2rem"} alt="like" />
+                        <Image src={bookmark} width={"2rem"} height={"2rem"} alt="bookmark" />
+                    </div>
+                    <div className="post-share">
+                        <Image src={repost} width={"2rem"} height={"2rem"} alt="repost" />
+                        <Image src={share} width={"2rem"} height={"2rem"} alt="share" />
+                    </div>
                 </div>
             </div>
 
-            <div className="post-body">
-                <Image src={test} width={600} alt="post"/>
-            </div>
-
-            <div className="post-footer">
-                <div className="post-action">
-                    <Image src={like} width={"2rem"} height={"2rem"} alt="like"/>
-                    <Image src={bookmark} width={"2rem"} height={"2rem"} alt="bookmark"/>
-                </div>
-                <div className="post-share">
-                    <Image src={repost} width={"2rem"} height={"2rem"} alt="repost"/>
-                    <Image src={share} width={"2rem"} height={"2rem"} alt="share"/>
-                </div>
-            </div>
         </div>
+
     )
 }
