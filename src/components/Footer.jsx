bc3e7b3324474290f9280950e5f36d3b6baa0efe
shraaavss <shravs2707@gmail.com>
2024-03-02 17:20:55 +0530

    migrated to NextJS

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+import "../output.css"
+
+export default function Footer(){
+
+    return(
+        <footer className="font-bold text-2xl"> 
+            <h1>This is a footer</h1>
+        </footer>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import home from './assets/homeIcon.svg';
+import search from './assets/searchIcon.svg';
+import prof from './assets/ProfileIcon.svg';
+
+export default function Header() {
+  return (
+    <div>
+        <header className="main-wrap">
+            <nav className="nav-bar">
+                <Link href={"/"}><Image width={40} className="nav-img" src={home} alt="home" /></Link>
+                <Link href={"/search"}><Image width={40} className="nav-img" src={search} alt="search" /></Link>
+                <Link href={"/profile"}><Image width={40} className="nav-img" src={prof} alt="profile" /></Link>
+            </nav>
+        </header>
+    </div>
+  )
+}
