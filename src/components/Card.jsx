8ddecb3ba407e9d1c301efc4ce4ef0e6997e4ac6
snehguptafr <guptasneh302004@gmail.com>
2024-03-02 19:03:36 +0530

    card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import Link from "next/link";
+import appImg from "../assets/appImg.svg";
+import snoozeImg from "../assets/snoozeImg.svg";
+import temp from "../assets/homeIcon.svg"
+import test from "../assets/test.jpeg"
+import bookmark from "../assets/bookmark.svg";
+import like from "../assets/like.svg"
+import repost from "../assets/repost.svg";
+import share from "../assets/share.svg"
+
+export default function Post(props){
+
+    return(
+        <div className="post">
+            <div className="post-header">
+                <div className="post-user">
+                    <Image src={temp} width={"2rem"} height={"2rem"} alt="userImg"/>
+                    <span>Sneh Gupta</span>
+                </div>
+                <div className="post-control">
+                    <Image src={appImg} width={"2rem"} height={"2rem"} alt="appImg"/>
+                    <Image src={snoozeImg} width={"2rem"} height={"2rem"} alt="snoozeImg"/>
+                </div>
+            </div>
+
+            <div className="post-body">
+                <Image src={test} width={550} alt="post"/>
+            </div>
+
+            <div className="post-footer">
+                <div className="post-action">
+                    <Image src={like} width={"2rem"} height={"2rem"} alt="like"/>
+                    <Image src={bookmark} width={"2rem"} height={"2rem"} alt="bookmark"/>
+                </div>
+                <div className="post-share">
+                    <Image src={repost} width={"2rem"} height={"2rem"} alt="repost"/>
+                    <Image src={share} width={"2rem"} height={"2rem"} alt="share"/>
+                </div>
+            </div>
+        </div>
+    )
+}
