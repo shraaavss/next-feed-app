a229889a3ceac16504640211175415c676cd78ea
shraaavss <shravs2707@gmail.com>
2024-03-03 12:09:28 +0530

    blocky collections

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 function Collections() {
   return (
-    <div>
-        
+    <div className='cards'>
+      <div className="card block">TWO</div>
+      <div className="card block">THREE</div>
+      <div className="card block">ONE</div>
+      <div className="card block">FOUR</div>
+      <div className="card block">FIVE</div>
+      <div className="card block">SIX</div>
+      <div className="card block">SEVEN</div>
+      <div className="card block">EIGHT</div>
+      <div className="card block">NINE</div>
+      <div className="card block">TEN</div>
+      <div className="card block">ELEVEN</div>
+      <div className="card block">TWELVE</div>
+
+
     </div>
   )
 }
