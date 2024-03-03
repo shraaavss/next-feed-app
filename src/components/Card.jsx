f21658707e3474376ed788f3080f63f3ba8953c9
snehguptafr <guptasneh302004@gmail.com>
2024-03-03 11:23:30 +0530

    like+bookmrk btn

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,10 @@ import appImg from "../assets/appImg.svg";
 import snoozeImg from "../assets/snoozeImg.svg";
 import temp from "../assets/homeIcon.svg"
 import test from "../assets/test.jpeg"
-import bookmark from "../assets/bookmark.svg";
-import like from "../assets/like.svg"
+import Bookmark from "../assets/bookmark.svg";
+import Bookmarked from "../assets/bookmarked.svg"
+import Like from "../assets/like.svg"
+import Liked from "../assets/liked.svg";
 
 import repost from "../assets/repost.svg";
 import share from "../assets/share.svg"
@@ -13,6 +15,7 @@ import { useState, useEffect } from "react";
 
 export default function Post(props) {
     const [liked, setLiked] = useState(false)
+    const [bookmarked, setBookmarked] = useState(false)
 
     return (
         <div className="post-wrap">
@@ -35,8 +38,16 @@ export default function Post(props) {
 
                 <div className="post-footer">
                     <div className="post-action">
-                        <svg onClick={()=> setLiked(!liked)} style={liked?{fill: "red"}:{fill: "black"}} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="m480-120-58-52q-101-91-167-157T150-447.5Q111-500 95.5-544T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447.5Q771-395 705-329T538-172l-58 52Zm0-108q96-86 158-147.5t98-107q36-45.5 50-81t14-70.5q0-60-40-100t-100-40q-47 0-87 26.5T518-680h-76q-15-41-55-67.5T300-774q-60 0-100 40t-40 100q0 35 14 70.5t50 81q36 45.5 98 107T480-228Zm0-273Z"/></svg>
-                        <Image src={bookmark} width={"2rem"} height={"2rem"} alt="bookmark" />
+                        {liked?
+                        <Image onClick={()=>setLiked(!liked)} src={Liked} width={"2rem"} height={"2rem"} alt={"Liked"}/>
+                        :
+                        <Image onClick={()=>setLiked(!liked)} src={Like} width={"2rem"} height={"2rem"} alt={"Like"}/>}
+                        {/* <Image src={Liked} width={"2rem"} height={"2rem"} alt={"liked"}/> */}
+                        {bookmarked?
+                        <Image onClick={()=>setBookmarked(!bookmarked)} src={Bookmarked} width={"2rem"} height={"2rem"} alt={"Bookmarked"}/>
+                        :
+                        <Image onClick={()=>setBookmarked(!bookmarked)} src={Bookmark} width={"2rem"} height={"2rem"} alt={"Bookmark"}/>}
+                        {/* <Image src={bookmark} width={"2rem"} height={"2rem"} alt="bookmark" /> */}
                     </div>
                     <div className="post-share">
                         <Image src={repost} width={"2rem"} height={"2rem"} alt="repost" />
