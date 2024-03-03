143bf28aa6fd1bcba56202b1d2680812601a4255
snehguptafr <guptasneh302004@gmail.com>
2024-03-03 11:53:59 +0530

    style tweaks

--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -5,9 +5,9 @@ import Image from 'next/image';
 function Search() {
     return (
         <div>
-            <div class="wrapper block">
-                <input />
-                <button class="block"><Image width={40} className="nav-img" src={searchIcon} alt="search" /></button>
+            <div style={{display: "flex"}} class="wrapper block">
+                <input style={{fontSize: "1.7rem", padding: "0.3rem", textAlign:"center"}} />
+                <button style={{width:"3rem", height:"3rem"}} class="block"><Image width={32} className="nav-img" src={searchIcon} alt="search" /></button>
             </div>
 
         </div>
